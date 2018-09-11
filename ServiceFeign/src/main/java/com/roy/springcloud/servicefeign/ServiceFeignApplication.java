97c808cff027124d135cca44a0e0ae1c3bd01bbd
liurj <liuranjun@sinopatho.com>
2018-09-11 11:21:03 +0800

    熔断器监控：HystrixDashboard

--- ServiceFeign/src/main/java/com/roy/springcloud/servicefeign/ServiceFeignApplication.java
+++ ServiceFeign/src/main/java/com/roy/springcloud/servicefeign/ServiceFeignApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableEurekaClient
-@EnableFeignClients
+@EnableHystrixDashboard //开启基于注解的熔断器仪表盘功能
+@EnableEurekaClient //开启基于注解的Eureka客户端功能
+@EnableFeignClients //开启基于注解的Feign客户端功能
 @SpringBootApplication
 public class ServiceFeignApplication {
 
--- ServiceRibbon/src/main/java/com/roy/springcloud/serviceribbon/ServiceRibbonApplication.java
+++ ServiceRibbon/src/main/java/com/roy/springcloud/serviceribbon/ServiceRibbonApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard //开始基于注解的熔断器仪表盘
 @EnableHystrix //开启基于注解的熔断器Hystrix
 @EnableEurekaClient //开启基于注解的EurekaClient
 @SpringBootApplication
