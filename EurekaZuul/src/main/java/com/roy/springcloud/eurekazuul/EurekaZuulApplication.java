2a7f4074b9e74109cff6062bf61861c1347d6770
liurj <liuranjun@sinopatho.com>
2018-09-11 15:00:39 +0800

    路由网关Zuul

---
+++ EurekaZuul/src/main/java/com/roy/springcloud/eurekazuul/EurekaZuulApplication.java
@@ -0,0 +1,16 @@
+package com.roy.springcloud.eurekazuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaZuulApplication.class,args);
+    }
+}
