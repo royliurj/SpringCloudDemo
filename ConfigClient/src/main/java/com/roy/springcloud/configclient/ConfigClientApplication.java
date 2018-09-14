5d717577da819e538b78c8eb7fe487e5b0f6be1b
liurj <liuranjun@sinopatho.com>
2018-09-14 11:42:36 +0800

    消息总线：SpringCloud Bus 调试未通过

--- ConfigClient/src/main/java/com/roy/springcloud/configclient/ConfigClientApplication.java
+++ ConfigClient/src/main/java/com/roy/springcloud/configclient/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.roy.springcloud.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//@RefreshScope //开启不重启服务的情况下更新配置
 @EnableEurekaClient
 @SpringBootApplication
 public class ConfigClientApplication {
